397c16dbd479c31dc539a103d719924deb87a3ed
Janez Franc Vratanar <jvratanar@gmail.com>
2024-02-05 22:23:26 +0100

    Improved docker configuration.

---
+++ src/test/java/tech/vratanar/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package tech.vratanar;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
